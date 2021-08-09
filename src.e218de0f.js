d81fa15eeef38ad1236821a3c567e9a8a467a874
iamaelita <73550495+iamaelita@users.noreply.github.com>
2021-08-09 08:41:20 +0000

    Deploying to gh-pages from @ iamaelita/goit-js-hw-10-food-service@9d96127e604c34d98e68966404e783d3b1eb9d86 🚀

--- src.e218de0f.js
+++ src.e218de0f.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"../styles.css":"Tnu0"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.e218de0f.js.map
+//# sourceMappingURL=/goit-js-hw-10-food-service/src.e218de0f.js.map
